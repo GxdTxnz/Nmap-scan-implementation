32f9561a2ee07c516075ee3f9c5bd886e905cd67
GxdTxnz <don22433@gmail.com>
2023-10-30 22:43:20 +0300

    улучшено определние сервиса порта и добавлен вывод вендора по MAC

--- mac.py
+++ mac.py
@@ -1,16 +1,33 @@
 from scapy.all import *
 from params import *
 
+def load_nmap_mac_prefixes():
+    nmap_services = {}
+    try:
+        with open('/usr/share/nmap/nmap-mac-prefixes', 'r') as file:
+            for line in file:
+                if not line or ' ' not in line:
+                    continue
+                prefix, vendor = line.split(' ', 1)
+                nmap_services[prefix] = vendor.strip()
+        return nmap_services
+    except Exception as e:
+        print(f"Ошибка при загрузке nmap-services: {e}")
+        return {}
+
 
 def get_mac_address(target_host):
     
+    nmap_services = load_nmap_mac_prefixes()
     mac_address = None
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     arp = ARP(pdst=target_host)
     packet = ether/arp
     result = srp(packet, timeout=3, verbose=0)[0]
-
     for _, received_packet in result:
         mac_address = received_packet[ARP].hwsrc
-        mac_upper = mac_address.upper()
-    print(f"MAC-адрес: {mac_upper}")
+
+    mac_address_upper = mac_address.upper()
+    mac_prefix = mac_address.upper().replace(':', '')[:6]
+    vendor = nmap_services.get(mac_prefix, None)
+    print(f"MAC-адрес: {mac_address_upper} ({vendor})")
--- tcp_CON_scan.py
+++ tcp_CON_scan.py
@@ -3,6 +3,46 @@ from scapy.all import *
 from params import *
 
 
+
+def load_nmap_services():
+    nmap_services = {}
+    try:
+        with open('/usr/share/nmap/nmap-services', 'r') as file:
+            for line in file:
+                if not line.startswith('#') and '/' in line:
+                    parts = line.split()
+                    service, port_protocol = parts[0], parts[1]
+                    port, protocol = port_protocol.split('/')
+                    if protocol == 'tcp':
+                        nmap_services[int(port)] = service
+        return nmap_services
+    except Exception as e:
+        print(f"Ошибка при загрузке nmap-services: {e}")
+        return {}
+
+
+def guess_service(target_host, port):
+    nmap_services = load_nmap_services()
+
+    # Проверить, есть ли порт в базе данных nmap-services
+    if port in nmap_services:
+        return nmap_services[port]
+
+    # Если нет, попробуем отправить HTTP GET запрос и проверить наличие HTTP-ответа
+    request = IP(dst=target_host)/TCP(dport=port, flags="S")
+    response = sr1(request, timeout=1, verbose=0)
+
+    if response is not None:
+        if response.haslayer(HTTP):
+            return "HTTP"
+        elif response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+            return "unknown"
+        else:
+            return "фильтруемый"
+    else:
+        return "фильтруемый"
+
+
 def tcp_connect_scan(target_host, port):
     global open_ports, closed_ports, filtered_ports
 
@@ -16,7 +56,6 @@ def tcp_connect_scan(target_host, port):
     except socket.error:
         service = "unknown"
 
-    
     if response is not None and response.haslayer(TCP):
         if response.getlayer(TCP).flags == 0x12:
             open_ports.append(port)
@@ -25,5 +64,6 @@ def tcp_connect_scan(target_host, port):
             closed_ports += 1 
             print(f"{port}/tcp: Закрыт ({service})")
     elif response is None:
-        filtered_ports.append(port) 
+        filtered_ports.append(port)
+        service = guess_service(target_host, port)
         print(f"{port}/tcp: Фильтруемый ({service})")
