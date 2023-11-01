76c165d8fb1b4bace58b9d4ec6d52fa5f2e0c632
GxdTxnz <don22433@gmail.com>
2023-11-02 01:03:39 +0300

    tcp_ACK доведен до ума. НЕ ЗАБУДЬ ИСПРАВИТЬ main

--- tcp_ACK_scan.py
+++ tcp_ACK_scan.py
@@ -1,22 +1,28 @@
 from scapy.all import *
 from params import *
-
+from service import *
 
 def tcp_ack_scan(target_host, port):
-    global closed_ports, unfiltered, filtered_ports
+    global unfiltered, filtered_ports
 
     ip_packet = IP(dst=target_host)
     tcp_packet = TCP(dport=port, flags="A")
     packet = ip_packet / tcp_packet
     response = sr1(packet, timeout=1, verbose=0)
-    if response:
+    service = guess_service(target_host, port)
+
+    if response is not None:
         if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x14:
-            closed_ports += 1
-            print(f"Порт {port}: Закрыт")
+            unfiltered.append(port)
+            print(f"{port}/tcp: Нефильтруемый ({service})")
+        elif response.haslayer(ICMP) and response.getlayer(ICMP).type == 3 and response.getlayer(ICMP).code in [1, 2, 3, 9, 10, 13]:
+            filtered_ports.append(port)
+            print(f"{port}/tcp: Фильтруемый ({service})")
         else:
             unfiltered.append(port)
-            print(f"Порт {port}: Нефильтруемый (Открытый или Закрытый)")
+            print(f"{port}/tcp: Нефильтруемый ({service})")
     else:
+        unfiltered.append(port)
         filtered_ports.append(port)
-        print(f"Порт {port}: Фильтруемый")
+        print(f"{port}/tcp: Фильтруемый ({service})")        
 
