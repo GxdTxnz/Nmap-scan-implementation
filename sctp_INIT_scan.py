16711740e93c3a3833d03422302f549300b049f7
GxdTxnz <don22433@gmail.com>
2023-11-08 18:04:15 +0300

    some updates

--- sctp_INIT_scan.py
+++ sctp_INIT_scan.py
@@ -9,8 +9,11 @@ def sctp_INIT(target_host, port):
 	ip_packet = IP(dst=target_host)
 	sctp_packet = SCTP(dport=port, flags="S")
 	packet = ip_packet / sctp_packet
-	responses = []
-    for _ in range(retries):
-        response = sr1(packet, timeout=2, verbose=0)
-        if response is not None:
-            responses.append(response)
+    response = sr1(packet, timeout=2, verbose=0)
+    service = guess_service(target_host, port)
+    
+    if response is not None:
+    	if response.haslayer(SCTP) and response.getlayer(SCTP).flags == 0x12:
+    		open_ports.append(port)
+			print(f"{port}/tcp: Открыт ({service})")
+		elif 
