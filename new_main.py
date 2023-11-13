cf7935441cc7c0446c473a0632728b5968dd5968
GxdTxnz <don22433@gmail.com>
2023-11-14 02:17:11 +0300

    new_main func has been updated(need fix)

--- new_main.py
+++ new_main.py
@@ -6,6 +6,7 @@ from tcp_ACK_scan import tcp_ack_scan
 from tcp_CON_scan import tcp_connect_scan
 from tcp_SYN_scan import tcp_syn_scan
 from udp_scans import udp_scan
+from sctp_INIT_scan import sctp_init_scan
 from sctp_COOKIE_scan import sctp_ce_scan
 from mac import get_mac_address
 from params import *
@@ -14,68 +15,87 @@ def initial_start():
     print(f"Начало сканирования {target_host} на портах от {start_port} до {end_port}...\n")
 
 def output(scan_type):
-    global open_ports, closed_ports, open_or_filtered_ports, filtered_ports, unfiltered
+    global open_ports, closed_ports, filtered_ports, unfiltered_ports, open_or_filtered_ports
 
     initial_start()
 
-    scan_functions = {
-        'sS': {
-            'name': "TCP SYN",
-            'func': tcp_syn_scan
-        },
-        'sT': {
-            'name': "TCP Connect",
-            'func': tcp_connect_scan
-        },
-        'sA': {
-            'name': "TCP ACK",
-            'func': tcp_ack_scan
-        },
-        'sY': {
-            'name': "UDP",
-            'func': udp_scan
-        },
-        'sZ': {
-            'name': "SCTP COOKIE ECHO",
-            'func': sctp_ce_scan
-        }
+    scan_funcs = {
+        'sS': tcp_syn_scan,
+        'sT': tcp_connect_scan,
+        'sA': tcp_ack_scan,
+        'sU': udp_scan,
+        'sY': sctp_init_scan,
+        'sZ': sctp_ce_scan
     }
 
-    if scan_type in scan_functions:
-        scan_info = scan_functions[scan_type]
-        scan_name = scan_info['name']
-        scan_func = scan_info['func']
+    if scan_type in scan_funcs:
+        scan_name = scan_funcs[scan_type].__name__
+        scan_func = scan_funcs[scan_type]
 
         for port in range(start_port, end_port + 1):
             scan_func(target_host, port)
 
         get_mac_address(target_host)
+
         print(f"\n{scan_name} сканирование завершено.\n")
-        print(f"Общее количество открытых портов: {len(open_ports)}")
-        print(f"Список открытых портов: {open_ports}")
 
-        if scan_type == 'sU':
-            print(f"Общее количество фильтруемых портов: {len(filtered_ports)}")
-            print(f"Список фильтруемых портов: {filtered_ports}")
+        labels = {
+            'sS': ['open_ports', 'closed_ports', 'filtered_ports'],
+            'sC': ['open_ports', 'closed_ports', 'filtered_ports'],
+            'sA': ['unfiltered', 'filtered_ports'],
+            'sU': ['open_ports', 'filtered_ports', 'open_or_filtered_ports', 'closed_ports'],
+            'sI': ['open_ports', 'closed_ports', 'filtered_ports'],
+            'sE': ['filtered_ports', 'open_or_filtered_ports', 'closed_ports']
+        }
 
-        if scan_type == 'sA':
-            print(f"Общее количество нефильтруемых портов: {len(unfiltered)}")
-            print(f"Список нефильтруемых портов: {unfiltered}")
+        for label in labels[scan_type]:
+            value = globals()[label]
+            if isinstance(value, list):
+                print(f"Общее количество {label} портов: {len(value)}")
+                print(f"Список {label} портов: {value}")
+            else:
+                print(f"Общее количество {label} портов: {value}")
+                print(f"Список {label} портов: {value}")
 
-        if scan_type == 'sU':
-            print(f"Общее количество открытых/фильтруемых портов: {len(open_or_filtered_ports)}")
-            print(f"Список открытых/фильтруемых портов: {open_or_filtered_ports}")
-
-        print(f"Общее количество закрытых портов: {len(closed_ports)}")
 
     else:
-        print("Выбран некорректный тип сканирования.")
+        print()
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Network Scanner")
-    parser.add_argument("scan_type", choices=['sS', 'sT', 'sA', 'sU', 'sZ'], help="Type of scan (1 - TCP SYN, 2 - TCP Connect, 3 - TCP ACK, 4 - UDP, 6 - SCTP COOKIE ECHO)")
+    parser = argparse.ArgumentParser(description="Скрипт для сетевого сканирования.")
+    parser.add_argument("scan_type", choices=['sS', 'sC', 'sA', 'sU', 'sI', 'sE'], help="Тип сканирования")
+    parser.add_argument("-t", "--target", help="Целевой хост")
+    parser.add_argument("-p", "--ports", help="Порты для сканирования")
+
     args = parser.parse_args()
+
+    global target_host, start_port, end_port
+    
+    target_host = args.target
+    ports = args.ports
+
+    if ports:
+        start_port, end_port = map(int, ports.split('-'))
+    else:
+        start_port, end_port = 1, 1024
+
     output(args.scan_type)
+
+'''
+    if args.ports:
+        if ',' in args.ports:
+            ports = [int(port) for port in args.ports.split(',')]
+            start_port, end_port = min(ports), max(ports)
+        elif '-' in args.ports:
+            start, end = map(int, args.ports.split('-'))
+            start_port, end_port = min(start, end), max(start, end)
+        else:
+            start_port = end_port = int(args.ports)
+    else:
+        start_port, end_port = 1, 1024
+'''
+
 
 if __name__ == "__main__":
     main()
