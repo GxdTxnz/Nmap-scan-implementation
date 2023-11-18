43ed8bf800b26007c0217613710ab56f89970b1e
GxdTxnz <don22433@gmail.com>
2023-11-18 18:47:40 +0300

    1. Функция main изменена. Теперь работа осуществляется с помощью ключей.

--- params.py
+++ params.py
@@ -1,7 +1,3 @@
-#target_host = input("Введите target ip: ")
-start_port = 130
-end_port = 140
-
 open_ports = []
 open_or_filtered_ports = []
 unfiltered = []
--- params2.py
+++
@@ -1,9 +0,0 @@
-#target_host = input("Введите target ip: ")
-#start_port = 130
-#end_port = 140
-
-open_ports = []
-open_or_filtered_ports = []
-unfiltered = []
-filtered_ports = []
-closed_ports = 0
